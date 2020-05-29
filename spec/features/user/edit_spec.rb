c2e4fc9b4aa0559d8c7481c048a9b8c55a0e7ad4
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-05-29 14:44:38 -0600

    Update test with correct css selectors

--- spec/features/user/edit_spec.rb
+++ spec/features/user/edit_spec.rb
@@ -4,16 +4,20 @@ RSpec.describe "As a User" do
   xit "I see a button to edit my profile" do
   end
   it "I can edit my own details" do
+    user = create(:user)
+    allow_any_instance_of(ApplicationController).
+    to receive(:current_user).
+    and_return(user)
 
     visit '/user/edit'
     expect(page).to have_content("Edit User Details")
 
-    expect(".email").to have_content(user.email)
-    expect(".name").to have_content(user.name)
+    expect(find("#user_name").value).to eq(user.name)
+    expect(find("#user_email").value).to eq(user.email)
 
-    fill_in "email", with: "new_example@example.com"
-    fill_in "name", with: "George Poolis"
-    click_on "submit"
+    fill_in "user[name]", with: "George Poolis"
+    fill_in "user[email]", with: "new_example@example.com"
+    click_on "commit"
 
     user.reload
     expect(user.name).to eq("George Poolis")
