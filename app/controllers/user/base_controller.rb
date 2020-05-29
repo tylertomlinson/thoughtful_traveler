b0ec5d670df3c3bb7c905d3559f41dcc73e1555a
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-05-29 14:48:58 -0600

    Add user controllers

--- app/controllers/user/base_controller.rb
+++ app/controllers/user/base_controller.rb
@@ -1,3 +1,8 @@
 class User::BaseController < ApplicationController
   # code
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
--- app/controllers/user/profile_controller.rb
+++ app/controllers/user/profile_controller.rb
@@ -1,5 +1,16 @@
 class User::ProfileController < User::BaseController
   def edit
+    @user = current_user
+  end
+
+  def update
+    begin
+      current_user.update!(user_params)
+      redirect_to user_path
+    rescue ActiveRecord::RecordInvalid
+      flash[:error] = errors.messages
+      render edit
+    end
   end
 
 end
