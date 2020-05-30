82d29e8f206d487bc854a2b9ed13d9ae0a316681
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-05-30 11:32:02 -0600

    Add from_omniauth to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ApplicationRecord
   has_many :trips
+
+  def self.from_omniauth(auth)
+    where(email: auth.info.email).first_or_initialize do |user|
+      user.user_name ||= auth.info.name
+      user.email = auth.info.email
+      user.password_digest = SecureRandom.hex
+      if !user.name
+        user.name = auth.info.name
+      end
+    end
+  end
 end
