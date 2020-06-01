5ae42abdb648068ec1f93c2f8de4783696814c02
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-06-01 09:51:42 -0600

    Add sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,12 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-# ActionMailer::Base.smtp_settings = {
-# :user_name => 'thoughtfultravelerdev@gmail.com',
-# :password => 'PAssword123',
-# :domain => 'localhost:3000',
-# :address => 'smtp.sendgrid.net',
-# :port => 465,
-# :authentication => :plain,
-# :enable_starttls_auto => true
-# }
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'thoughtfultravelerdev@gmail.com',
+  :password => 'PAssword123',
+  :domain => 'localhost:3000',
+  :address => 'smtp.sendgrid.net',
+  :port => 465,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
