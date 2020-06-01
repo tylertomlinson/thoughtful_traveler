7b32ec024a7dddcb62452f30abbf5039fa5cbbbc
Alex Lynch <mlynch5187@gmail.com>
2020-05-31 19:27:04 -0600

    Create visitor trips new spec

---
+++ spec/features/visitor/trips/new_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe 'As a visitor' do
+  describe 'I create a new trip from the welcome page' do
+    it 'I see a form to create a new trip' do
+
+      visit '/'
+
+      click_on 'Create Trip'
+
+      expect(current_path).to eq("/user/trips/new")
+
+      expect(page).to have_content('Trip Preferences')
+      expect(page).to have_content('Log in with Google')
+
+      fill_in 'location', with: 'Denver, co'
+      fill_in 'start_date', with: '2525/01/01'
+      fill_in 'end_date', with: '2525/12/12'
+      within '.cuisine_options' do
+        check 'indian'
+        check 'mexican'
+        check 'italian'
+      end
+      within '.attraction_options' do
+        check 'music'
+        check 'sports'
+      end
+      click_on 'Create My Trip'
+    end
+    expect(page).to have_content('You must log in to create a trip')
+  end
+end
