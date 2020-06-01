f4fcf4548a45152357543d03f655d58487cfc2d3
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-05-31 22:00:22 -0600

    Add email controller

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -2,11 +2,10 @@ class EmailController < ApplicationController
 
   def create
     recipients_email = params[:recipients_email]
-    email_info = {user: current_user,
-                 }
+    email_info = { user: current_user }
   begin
     TripMailer.send_email(email_info, recipients_email).deliver_now
-    flash[:notice] = "Your email was successfully sent"
+    flash[:notice] = 'Your email was successfully sent'
   rescue StandardError
     flash[:message] = errors.message
   end
