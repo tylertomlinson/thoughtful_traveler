f350188be532f43cea4a9c962ba75450cac2f109
Jesse Gietzen <gietzenjesse@gmail.com>
2020-05-31 20:15:07 -0600

    add event show route

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     patch '/', to: 'profile#update'
     get '/', to: 'dashboard#index'
 
-    resources :trips, only: [:index, :new, :create, :edit]
+    resources :trips, only: [:index, :new, :create, :show, :edit]
     resources :trip_events, only: [:update]
     resources :upcoming_trips
     resources :past_trips
