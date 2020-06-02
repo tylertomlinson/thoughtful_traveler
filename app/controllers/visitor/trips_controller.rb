9017dabd08fa1b7ba3477230a9cd4a658243dd8a
Alex Lynch <mlynch5187@gmail.com>
2020-06-02 10:51:33 -0600

    Visitors are redirected to new form page and asked to sign in to create trip

--- app/controllers/visitor/trips_controller.rb
+++ app/controllers/visitor/trips_controller.rb
@@ -1,7 +1,7 @@
 class Visitor::TripsController < Visitor::BaseController
 
   def create
-    render :new
+    redirect_to '/visitor/trips/new'
     flash[:notice] = 'You must sign in to create a trip'
   end
 
