6e792f1b5719811c6063a96a5d88030d03b32e12
Alex Lynch <mlynch5187@gmail.com>
2020-06-04 15:17:34 -0600

    Add past trip button to past trips index

--- spec/features/user/trips/past/index_spec.rb
+++ spec/features/user/trips/past/index_spec.rb
@@ -4,16 +4,17 @@ RSpec.describe 'Past Trips Index' do
   describe 'As a logged in user' do
     describe 'On the past trips index' do
       before(:each) do
-        user = create(:user)
-        trip = user.trips.create(attributes_for(:trip))
+        @user = create(:user)
+        @trip = @user.trips.create(attributes_for(:trip))
 
-        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
 
         visit '/user/trips/past/trips'
       end
 
-      it 'I can see all past trips' do
-
+      it 'I can see all past trips as links' do
+        save_and_open_page
+        expect(page).to have_button("Trip to #{@trip.location}")
       end
     end
   end
